555ed401ba6a768c1ff50245770fdeb2f7a0d8ba
Andrew Graham <andrew@firehawkvfx.com>
2021-04-09 08:46:02 +0930

    fix broken retry - do not use stdout

--- modules/node-centos7-houdini/user-data-install-deadline-worker.sh
+++ modules/node-centos7-houdini/user-data-install-deadline-worker.sh
@@ -8,6 +8,7 @@ deadlineuser_name="deadlineuser"
 resourcetier="dev"
 installers_bucket="software.$resourcetier.firehawkvfx.com"
 deadline_version="10.1.9.2"
+example_role_name="rendernode-vault-role"
 
 # User Vars: Set by terraform template
 deadlineuser_name="${deadlineuser_name}"
@@ -16,13 +17,18 @@ installers_bucket="${installers_bucket}"
 deadline_version="${deadline_version}"
 
 # Script vars (implicit)
-VAULT_ADDR=https://vault.service.consul:8200
+export VAULT_ADDR=https://vault.service.consul:8200
 client_cert_file_path="/opt/Thinkbox/certs/Deadline10RemoteClient.pfx"
 client_cert_vault_path="$resourcetier/deadline/client_cert_files$client_cert_file_path"
 installer_file="install-deadline-worker.sh"
 installer_path="/home/$deadlineuser_name/Downloads/$installer_file"
 
 # Functions
+function log {
+ local -r message="$1"
+ local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+ >&2 echo -e "$timestamp $message"
+}
 function has_yum {
   [[ -n "$(command -v yum)" ]]
 }
@@ -34,23 +40,29 @@ function retry {
   local -r cmd="$1"
   local -r description="$2"
   attempts=5
+
   for i in $(seq 1 $attempts); do
-    echo "$description"
+    log "$description"
+
     # The boolean operations with the exit status are there to temporarily circumvent the "set -e" at the
     # beginning of this script which exits the script immediatelly for error status while not losing the exit status code
     output=$(eval "$cmd") && exit_status=0 || exit_status=$?
     errors=$(echo "$output") | grep '^{' | jq -r .errors
-    echo "$output"
+
+    log "$output"
+
     if [[ $exit_status -eq 0 && -z "$errors" ]]; then
       echo "$output"
       return
     fi
-    echo "$description failed. Will sleep for 10 seconds and try again."
+    log "$description failed. Will sleep for 10 seconds and try again."
     sleep 10
   done;
-  echo "$description failed after $attempts attempts. exit_status: $exit_status"
+
+  log "$description failed after $attempts attempts."
   exit $exit_status
 }
+
 function retrieve_file {
   echo "source var"
   local -r source_path="$1"
@@ -60,43 +72,33 @@ function retrieve_file {
     local -r target_path="$2"
   fi
   echo "response:"
-  local -r response=$(retry \
+  # local -r response=$(retry \
+  response=$(retry \
   "vault kv get -field=value $source_path/file" \
   "Trying to read secret from vault")
   echo "$response"
   echo "mkdir: $(dirname $target_path)"
-  sudo mkdir -p "$(dirname $target_path)" # ensure the directory exists
-  # echo $response | jq -r .data.data | sudo tee $target_path # retrieve full json blob to later pass permissions if required.
-  # echo "decode"
-  # # jq seems to fail decoding some certs, so we use python instead.
-  # decoded="$(blob=$response python -c \"import os,json; blob=os.environ['blob']; print( json.loads(blob)['data']['data']['file'] )\" | base64 --decode)"
-  # decoded="$(blob=$response python -c \"import os,json; print(os.environ['blob'])\")"
-  
-  # raw=$(echo "$response" | jq -r '.data.data.file')
-  # echo "decode"
-  # decode=$(echo "$raw" | base64 --decode)
-  # echo "write to file"
-  # echo "$decode" | sudo tee $target_path
-  # echo "write output"
-  # raw=$( python -c "import json; blob=json.loads($response); print( blob[\"data\"][\"data\"][\"file\"] )" )
-  echo "Check file path is writable"
-  if [[ ! -f "$target_path" ]]; then 
+  mkdir -p "$(dirname $target_path)" # ensure the directory exists
+  echo "Check file path is writable: $target_path"
+  if test -f "$target_path"; then
+    echo "File exists: ensuring it is writeable"
+    chmod u+w "$target_path"
     touch "$target_path"
   else
-    echo "Error: Path not writable: $target_path "
+    echo "Ensuring path is writeable"
+    touch "$target_path"
+    chmod u+w "$target_path"
+    # echo "Error: Path not writable: $target_path "
   fi
   if [[ -f "$target_path" ]]; then
-    sudo chmod u+w "$target_path"
+    chmod u+w "$target_path"
   else
     echo "Error: path does not exist, var may not be a file: $target_path "
   fi
   # sudo chmod u+w $target_path
+  echo "test decode"
+  echo "$response" | base64 --decode
   echo "Write file content: single operation"
-  # echo $(retry \
-  # "vault kv get -format=json $source_path" \
-  # "Trying to read secret from vault") | jq -r '.data.data.file' | base64 --decode > $target_path
-  # echo "Write file content from var"
-  # echo "$response" | jq -r '.data.data.file' | base64 --decode > $target_path
   echo "$response" | base64 --decode > $target_path
   if [[ ! -f "$target_path" ]] || [[ -z "$(cat $target_path)" ]]; then
     echo "Error: no file or empty result at $target_path"
@@ -146,6 +148,12 @@ printf "\n...Waiting for consul deadlinedb service before attempting to retrieve
 
 until consul catalog services | grep -m 1 "deadlinedb"; do sleep 1 ; done
 
+echo "Ensure REquired Directories exist with valid permissions"
+mkdir -p $(dirname $client_cert_file_path)                                                                
+chown $deadlineuser_name:$deadlineuser_name /opt/Thinkbox/
+chown $deadlineuser_name:$deadlineuser_name /opt/Thinkbox/certs/
+chmod u=rw,g=rw,o-rwx $client_cert_file_path
+
 ### Vault Auth IAM Method CLI
 retry \
   "vault login --no-print -method=aws header_value=vault.service.consul role=${example_role_name}" \
@@ -153,6 +161,7 @@ retry \
 echo "Aquiring vault data... $client_cert_vault_path to $client_cert_file_path"
 # Retrieve previously generated secrets from Vault.  Would be better if we can use vault as an intermediary to generate certs.
 retrieve_file "$client_cert_vault_path" "$client_cert_file_path"
+
 echo "Revoking vault token..."
 vault token revoke -self
 
