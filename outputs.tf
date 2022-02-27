ecaa1013165dac6c828a455689a1230c3f5db41f
Andrew Graham <queglay@gmail.com>
2022-02-27 23:28:37 +1030

    rename to coalesce

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "instance_name" {
   value = local.instance_name
 }
 output "private_ip" {
-  value = coelesce( module.node_centos7_houdini.private_ip, "")
+  value = coalesce( module.node_centos7_houdini.private_ip, "")
 }
 output "id" {
-  value = coelesce( module.node_centos7_houdini.id, "")
+  value = coalesce( module.node_centos7_houdini.id, "")
 }
 output "consul_private_dns" {
-  value = coelesce( module.node_centos7_houdini.consul_private_dns, "")
+  value = coalesce( module.node_centos7_houdini.consul_private_dns, "")
 }
