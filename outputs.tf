adfff2feda6b80a6f7e728e7744aa4e2f3c11d71
Andrew Graham <queglay@gmail.com>
2022-02-27 23:25:32 +1030

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "instance_name" {
   value = local.instance_name
 }
 output "private_ip" {
-  value = module.node_centos7_houdini.private_ip
+  value = coelesce( module.node_centos7_houdini.private_ip, "")
 }
 output "id" {
-  value = module.node_centos7_houdini.id
+  value = coelesce( module.node_centos7_houdini.id, "")
 }
 output "consul_private_dns" {
   value = coelesce( module.node_centos7_houdini.consul_private_dns, "")
