75e60c1de634772a9f4b725e36795258ef638edc
Andrew Graham <queglay@gmail.com>
2022-02-28 15:53:16 +1030

    use coalesce for output

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "instance_name" {
   value = local.instance_name
 }
 output "private_ip" {
-  value = length(module.node_centos7_houdini) > 0 ? coalesce( module.node_centos7_houdini.private_ip, "") : ""
+  value = coalesce( module.node_centos7_houdini.private_ip, "")
 }
 output "id" {
-  value = length(module.node_centos7_houdini) > 0 ? coalesce( module.node_centos7_houdini.id, "") : ""
+  value = coalesce( module.node_centos7_houdini.id, "")
 }
 output "consul_private_dns" {
-  value = length(module.node_centos7_houdini) > 0 ? coalesce( module.node_centos7_houdini.consul_private_dns, "") : ""
+  value = coalesce( module.node_centos7_houdini.consul_private_dns, "")
 }
