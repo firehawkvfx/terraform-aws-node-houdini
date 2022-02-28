1aee1a5d026030d695ec8764c392b608d2900334
Andrew Graham <queglay@gmail.com>
2022-02-28 16:10:02 +1030

    refer to an instance

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "instance_name" {
   value = local.instance_name
 }
 output "private_ip" {
-  value = coalesce( module.node_centos7_houdini.private_ip, "")
+  value = coalesce(module.node_centos7_houdini[0].private_ip, "")
 }
 output "id" {
-  value = coalesce( module.node_centos7_houdini.id, "")
+  value = coalesce(module.node_centos7_houdini[0].id, "")
 }
 output "consul_private_dns" {
-  value = coalesce( module.node_centos7_houdini.consul_private_dns, "")
+  value = coalesce(module.node_centos7_houdini[0].consul_private_dns, "")
 }
